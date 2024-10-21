cbc82da2ecea298afffb82bfece91911408d0b74
aleksteig <aleksteigen@gmail.com>
2024-10-21 13:23:16 +0200

    First commit

---
+++ week43.js
@@ -0,0 +1,78 @@
+// Hello.
+// This is a set of exercises.
+// The idea is to practice a few things at a time.
+// You do this by writing your answer after a task is given (see the example).
+// Use the concepts from our sylabus. 
+
+// DO NOT change the provided code unless the task specifically says to do so.
+
+/* -----------------------------------------------------------------------------
+    Task: Example
+    Write code to print all the names in the list, one name per line
+*/
+console.log("Task: Example");
+const people = ["Tony", "Christian", "Håkon"]
+
+for (let index = 0; index < people.length; index++) {
+    let person = people[index];
+    console.log(person);
+}
+
+/* -----------------------------------------------------------------------------
+    Task: A
+    create a function that "draws" a tree of a given height.
+    Example the following is a tree of height 5
+
+           5        *
+           4       ***
+           3      *****
+           2     *******
+           1        x
+*/
+console.log("Task: A");
+
+function drawTree(height){
+    
+}
+
+/* -----------------------------------------------------------------------------
+    Task: B
+
+    Write a function that "draws" an arrow of a given size.
+    Example: This is an arrow of size 3.
+
+    *
+    * *
+    * * *
+    * *
+    * 
+*/
+console.log("Task: B");
+
+/* -----------------------------------------------------------------------------
+    Task: C
+    Write a function that draws a box of n by m dimensions. Take into acount the diffrence in aspectratio.
+
+    Example: This is a aproximatly a 2 by 2 box. 
+    +--------+
+    |        |
+    |        |
+    +--------+
+*/
+console.log("Task: C");
+
+
+
+/* -----------------------------------------------------------------------------
+    Task: D
+    Write a function that returns true if a word is a Heterogram.   
+*/
+console.log("Task: D");
+
+
+
+/* -----------------------------------------------------------------------------
+    Task: E
+    Write a function that returns true if two words are Anagrams.
+*/
+console.log("Task: E");
